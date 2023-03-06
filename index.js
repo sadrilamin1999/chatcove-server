7daf9d4290f65f5c380ccca902e61ce04156d4d0
Sadril Amin <sadrilshuvo420@gmail.com>
2023-03-06 16:18:59 +0600

    register route added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const userRouter = require("./routes/userRoute");
 
 // express app
 const app = express();
@@ -11,12 +12,23 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+// endpoints
+app.use("/api/user", userRouter);
+
 //port
 const PORT = process.env.PORT || 4000;
 
+// routes
+app.get("/", (req, res) => {
+  res.json({ message: "Hello" });
+});
+
 // connect to db
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     // listening for request
     app.listen(PORT, (req, res) => {
