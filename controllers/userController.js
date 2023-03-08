3ae4e9a13b1d8bbf696a1cf7d430581a1c3bba3b
Sadril Amin <sadrilshuvo420@gmail.com>
2023-03-08 14:02:25 +0600

    user register controller added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,55 @@
 const userModel = require("../models/userModel");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
+const jwt = require("jsonwebtoken");
+
+// genarate token
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
+};
 
 // register user
-const registerUser = (req, res) => {
-  res.json({ message: "register" });
+const registerUser = async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const exist = await userModel.findOne({ email });
+    // user exist
+    if (exist) {
+      return res.status(400).json("Email already exist.");
+    }
+    // validation
+    if (!name || !email || !password) {
+      return res.status(400).json("All fields are required.");
+    }
+
+    if (!validator.isEmail(email)) {
+      return res.status(400).json("Invalied email");
+    }
+    if (!validator.isStrongPassword(password)) {
+      return res
+        .status(400)
+        .json(
+          "Password must be contain uppercase, lowercase, number, symbol and minimum 8+ chars."
+        );
+    }
+
+    // hash passworod
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    // create an user
+    const user = await userModel.create({ name, email, password: hash });
+
+    // create token
+    const token = createToken(user._id);
+
+    res
+      .status(200)
+      .json({ _id: user._id, name, email, password: user.password, token });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 };
 
 module.exports = {
